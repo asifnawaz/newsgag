1f6ac6e1104a68729056061acb1c0e65b99c123d
asifnawaz <iamasifnawaz@hotmail.com>
2021-05-27 23:42:35 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,9 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         schema: {
-       perPage: 20, // currently set to 100
-       requestConcurrency: 5, // currently set to 15
-       previewRequestConcurrency: 2, // currently set to 5
+       perPage: 10, // currently set to 100
+       requestConcurrency: 3, // currently set to 15
+       previewRequestConcurrency: 0, // currently set to 5
         },
         // the only required plugin option for WordPress is the GraphQL url.
         url:
